8a892ff863f3263fa3276cdd1d20505f1bd539a9
Mari Sinkkonen <Mari.Sinkkonen@tecnotree.com>
2022-12-29 16:18:57 +0200

    props practise

--- wmc/src/components/BookList.js
+++ wmc/src/components/BookList.js
@@ -1,40 +1,43 @@
 import React from "react";
 import "./BookList.css";
 
+const books = [
+    {
+        author: "Colleen Hoover",
+        title: "It Starts with Us: A Novel (It Ends with Us)",
+        img: "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/51VC+Vru96L._SX320_BO1,204,203,200_.jpg",
+    },
+    {
+        author: "Prince Harry The Duke of Sussex",
+        title: "Spare",
+        img: "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/41CSAHVHk+L._SX327_BO1,204,203,200_.jpg",
+    },
+];
+
 function BookList() {
-    const Book = () => {
-        return (
-            <article className="book">
-                <Image />
-                <Title />
-                <Author />
-            </article>
-        );
-    };
-
-    const Image = () => (
-        <img
-            src="https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/51VC+Vru96L._SX320_BO1,204,203,200_.jpg"
-            alt="Picture of bookcover with white flowers in water"
-        ></img>
-    );
-    const Title = () => <h2>It Starts with Us: A Novel (It Ends with Us)</h2>;
-    const Author = () => (
-        <h4 style={{ color: "#617d98", fontSize: "0.75", marginTop: "0.5rem" }}>
-            Colleen Hoover
-        </h4>
-    );
-
     return (
         <section className="booklist">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            {books.map(book => {
+                const { img, author, title } = book;
+                return <Book img={img} title={title} author={author} />;
+            })}
         </section>
     );
 }
+
+const Book = props => {
+    const { img, title, author } = props;
+    console.log(props);
+    const a = 6;
+    const b = 6;
+    return (
+        <article className="book">
+            <img src={props.img} alt={props.title} />
+            <h2>{props.title}</h2>
+            <h4>{props.author}</h4>
+            <h5>{a + b}</h5>
+        </article>
+    );
+};
 
 export default BookList;
